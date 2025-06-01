368988aa1c37138366036b1483d62d251a3ceb59
GollumBurukRupa <supriyadi185@gmail.com>
2025-06-02 04:41:11 +0700

    Add files via upload

--- cari2Dv1.1.sh
+++ cari2Dv1.1.sh
@@ -21,13 +21,13 @@ if [ "$jkata" -gt "1" ]
 then
 #echo -n $jkata > $dwp/jkata
 cat $dwp/input|busybox tr ' ' '\x0a' > $dwp/input2
-cat $dwp/input2|busybox awk -v jalur=$dwp '{print "cat " jalur "/buku2D.txt|grep -i \"" $0 "\""}' > $dwp/01-$(basename $0)
+cat $dwp/input2|busybox awk -v jalur=$dwp '{print "if [ ! -z \"\$(cat " jalur "/buku2D.txt|grep -i \"" $0 "\")\" ]\nthen\ncat " jalur "/buku2D.txt|grep -i \"" $0 "\"\nfi\nif [ -z \"\$(cat " jalur "/buku2D.txt|grep -i \"" $0 "\")\" ]\nthen\necho \"\"\necho \"Kata Kunci " $0 " gak ada di Kitab... hahaha.\"\nfi"}' > $dwp/01-$(basename $0)
 $(readlink /proc/$$/exe) $dwp/01-$(basename $0)
 fi
 
 if [ "$jkata" == "1" ]
 then
-busybox echo -e "#!$(readlink /proc/$$/exe)\ncat $dwp/buku2D.txt|grep -i \"$@\"" > $dwp/00-$(basename $0)
+busybox echo -e "#!$(readlink /proc/$$/exe)\nif [ ! -z \"\$(cat $dwp/buku2D.txt|grep -i \"$@\")\" ]\nthen\ncat $dwp/buku2D.txt|grep -i \"$@\"\nfi\nif [ -z \"\$(cat $dwp/buku2D.txt|grep -i \"$@\")\" ]\nthen\necho \"\"\necho \"Kata kunci $@ gak ada di kitab... hahaha\"\nfi" > $dwp/00-$(basename $0)
 $(readlink /proc/$$/exe) $dwp/00-$(basename $0)
 fi
 echo ""
--- cari3Dv1.1.sh
+++ cari3Dv1.1.sh
@@ -8,7 +8,7 @@ fi
 
 if [ ! -f "$dwp/buku3D.txt" ]
 then
-busybox echo -e "\n### Tidak ada buku 3D."
+busybox echo -e "\n### Tidak ada buku 3D.\nUnduh di github jendraljack\n"
 kill -9 $$
 fi
 busybox echo -e "\n### Skrip pencari kode binatang 3D ###\n"
@@ -21,13 +21,13 @@ if [ "$jkata" -gt "1" ]
 then
 #echo -n $jkata > $dwp/jkata
 cat $dwp/input|busybox tr ' ' '\x0a' > $dwp/input2
-cat $dwp/input2|busybox awk -v jalur=$dwp '{print "cat " jalur "/buku3D.txt|grep -i \"" $0 "\""}' > $dwp/01-$(basename $0)
+cat $dwp/input2|busybox awk -v jalur=$dwp '{print "if [ ! -z \"\$(cat " jalur "/buku3D.txt|grep -i \"" $0 "\")\" ]\nthen\ncat " jalur "/buku3D.txt|grep -i \"" $0 "\"\nfi\nif [ -z \"\$(cat " jalur "/buku3D.txt|grep -i \"" $0 "\")\" ]\nthen\necho \"\"\necho \"Kata Kunci " $0 " gak ada di Kitab... hahaha.\"\nfi"}' > $dwp/01-$(basename $0)
 $(readlink /proc/$$/exe) $dwp/01-$(basename $0)
 fi
 
 if [ "$jkata" == "1" ]
 then
-busybox echo -e "#!$(readlink /proc/$$/exe)\ncat $dwp/buku3D.txt|grep -i \"$@\"" > $dwp/00-$(basename $0)
+busybox echo -e "#!$(readlink /proc/$$/exe)\nif [ ! -z \"\$(cat $dwp/buku3D.txt|grep -i \"$@\")\" ]\nthen\ncat $dwp/buku3D.txt|grep -i \"$@\"\nfi\nif [ -z \"\$(cat $dwp/buku3D.txt|grep -i \"$@\")\" ]\nthen\necho \"\"\necho \"Kata kunci $@ gak ada di kitab... hahaha\"\nfi" > $dwp/00-$(basename $0)
 $(readlink /proc/$$/exe) $dwp/00-$(basename $0)
 fi
 echo ""
